3d4dabbce9d7fa066be6420334713e91b63f7c05
deskostadinova <91456206+deskostadinova@users.noreply.github.com>
2024-11-28 15:15:35 +0200

    updates to tasks 3 and 4

---
+++ Task3SalaryDeduction/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+bool isTabsParsed = false;
+int tabCount = 0;
+bool isSalaryParsed = false;
+int salaryCount = 0;
+const string siteFacebook = "facebook";
+const string siteInstagram = "instagram";
+const string siteReddit = "reddit";
+string typeOfSite = null;
+int fineCount = 0;
+int salaryDeduction = 0;
+int remainingSalary = 0;
+
+Console.WriteLine("Number of open tabs: ");
+
+while (!isTabsParsed)
+{
+    string tabCountInput=Console.ReadLine();
+    isTabsParsed = int.TryParse(tabCountInput, out tabCount) && tabCount >= 1 
+                                                             && tabCount <= 10;
+    if (isTabsParsed)
+    {
+        break;
+    }
+    Console.WriteLine("Please enter a number between 1 and 10: ");
+}
+
+Console.WriteLine("Salary: ");
+
+while (!isSalaryParsed)
+{
+    string salaryInput = Console.ReadLine();
+    isSalaryParsed = int.TryParse(salaryInput, out salaryCount) && salaryCount >= 700 
+                                                                && salaryCount <= 1500;
+    if (isSalaryParsed)
+    {
+        break;
+    }
+    
+    Console.WriteLine("Please enter a number between 700 and 1500: ");
+}
+
+Console.WriteLine("Type of site: ");
+
+while (typeOfSite != siteFacebook && typeOfSite != siteInstagram && typeOfSite != siteReddit)
+{
+    typeOfSite = Console.ReadLine().ToLower();
+    switch (typeOfSite)
+    {
+        case siteFacebook:
+            fineCount = 150;
+            break;
+        case siteInstagram:
+            fineCount = 100;
+            break;
+        case siteReddit:
+            fineCount = 50;
+            break;
+        default: 
+            Console.WriteLine("Please enter a valid type of site ");
+            break;
+    }
+}
+
+salaryDeduction = tabCount * fineCount;
+remainingSalary = salaryCount - salaryDeduction;
+
+if (remainingSalary > 0)
+{
+    Console.WriteLine("Remaining salary: " + remainingSalary);
+}
+else
+{
+    Console.WriteLine("You have lost your salary.");
+}
---
+++ Task4StepsToReachAGol/Program.cs
@@ -0,0 +1,60 @@
+﻿int stepsCount = 0;
+string goingHome = "going home";
+int stepGoal = 10000;
+
+while (stepsCount < stepGoal)
+{
+    Console.WriteLine("Number of steps: ");
+    string userInput = Console.ReadLine();
+    int stepsToAdd = 0;
+    
+    bool isNumber = int.TryParse(userInput, out stepsToAdd);
+
+    if (isNumber)
+    {
+        stepsCount += stepsToAdd;
+        if (stepsCount >= stepGoal)
+        {
+            Console.WriteLine($"You reached your goal with {stepsCount} steps! Great job!");
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"You added {stepsToAdd} steps to your total of {stepsCount}. Remaining: {stepGoal-stepsCount}");   
+        }
+    }
+    else
+    {
+        if (userInput.ToLower().Equals(goingHome))
+        {
+            Console.WriteLine("How many steps did you make going home?");
+            userInput = Console.ReadLine();
+            isNumber = int.TryParse(userInput, out stepsToAdd);
+            if (isNumber)
+            {
+                stepsCount += stepsToAdd;
+                if (stepsCount >= 10000)
+                {
+                    Console.WriteLine($"You reached your goal with {stepsCount}! Great job!!!");
+                    break;  
+                }
+                else
+                {
+                    Console.WriteLine($"You added {stepsToAdd} steps to your total of {stepsCount}. " +
+                                      $"You finish the day with remaining: {stepGoal-stepsCount} steps to reach the goal");
+                    break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid value");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid value");
+        }
+    }
+}
+
+
